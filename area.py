6f67b43e27df35a6be81c1dc960538295b00a0df
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-09-27 13:56:03 -0400

    edited

--- area.py
+++ area.py
@@ -1,10 +1,12 @@
-height = ("input the height: ")
+# Input the height and base as numbers
+height = float(input("Enter the height: "))
+base = float(input("Enter the base: "))
 
-base = ("input the base: ")
-
-print = (f"height, {height} ,base, {base}: ")
-
+# Calculate the area of the triangle
 area = 0.5 * height * base
 
-print = (f"area, {area}: ")
+# Display the results
 
+print(f"The area of the triangle is {area}.")
+
+print(f"The height is {height} and the base is {base}.")

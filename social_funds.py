e94d9ae1af125d84dba0c776606316e1e4a4c691
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-09-28 19:58:31 -0400

    edited

--- social_funds.py
+++ social_funds.py
@@ -1,7 +1,12 @@
-sex = input("What is your sex")
-if sex != "male":
-    print("Your the luckiest woman on earth:")
-          
-else:
-    print("get married ")
+while True:
+    sex = input("Are you male /female/other: ")
+    if sex ==  "male":
+        print("ok mr. your wellcome!")
+        break
+    elif sex != "male":
+        print("kindly report to reception for more information")
+    else:
+        print("Invalid input.kindly try Again!_")
+
+
     

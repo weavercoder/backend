5da5ebdc322579a5754ee8e584384e1474dc9fa5
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-09-28 19:11:47 -0400

    add_new

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+length = float(input("enter length: "))
+width = float(input("enter width: "))
+
+area = length * width
+print(f"The width is {width} and the length is {length} :- ")
+print(f"the area is {area}:   ")

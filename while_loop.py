b0510a323e3377d11d5f181d1ac054e36f90a451
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-22 05:54:25 -0500

    changes

--- while_loop.py
+++ while_loop.py
@@ -5,15 +5,17 @@ sum_of_multiples = 0
 number = 1
 
 while number < 1000:
-    if number % 3  == 0 or % 5  == 0:
     
-sum_of_multiples += number
+    if number % 3  == 0 or  number % 5  == 0:
+    
+    #Add multiples to the sum
+        sum_of_multiples += number
 
-#increase the counter by 1 in  order to move to the next number
+#increament of  the number for the next alteration
 number += 1
 
 
-print(f"the sum of multiples of 3 and 5:{sum_of_multiples}")
+print(f"the sum of multiples of 3 and 5 below 1000: {sum_of_multiples}")
 
 
     

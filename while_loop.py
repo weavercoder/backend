2fa613cabaa8906e9d1835c965b5e9458da9e386
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-22 05:10:23 -0500

    add_changes

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+#initialize a variable to store our multiples
+sum_of_multiples = 0
+sum_of_multiples = 0
+
+#set a counter
+number = 1
+
+while number <= 1000:
+  if number % 3  == 0 or 5 % == 0
+    
+sum_of_multiples += number
+
+#increase the counter by 1 in  order to move to the next number
+number += 1
+
+
+print(f"the sum of multiples of 3 and 5:{sum_of_multiples}")
+
+
+    
+    

a2cded36243ed8ef349b2d8730c1ca0b281bf5a9
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-22 12:07:08 -0500

    add_new

--- for_loop.py
+++ for_loop.py
@@ -1,9 +1,17 @@
 #for loop example using odd_even case scenerio
 
-start = str(input("enter the first number in the range"))
-end = str(input("enter the last number in the range"))
+start = int(input("enter the first number in the range"))
+end = int(input("enter the last number in the range"))
 
-for number in range (start,end)
-#to check even numbers on that range
+for number in range(start,end):
+       #to check even numbers on that range
  
-if  start
+    if  number % 2 == 0:
+        print("your number is even")
+    elif number % 2!= 0:
+        print("your number is odd")
+    else:
+        print("enter a valid number")
+
+              
+

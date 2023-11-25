325c68738f343d44f1659034ce96f11d74c8fe35
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-25 10:47:31 -0500

    Add_new

--- while_loop.py
+++ while_loop.py
@@ -1,26 +1,17 @@
+# A program that gets the sum of all multiples of 3 or 5 below 1000
 
-#A program that gets sum of all multiples of  3 or 5 below 1000
-
-#initialize a variable to store the sum
+# Initialize a variable to store the sum
 sum_of_multiples = 0
 
-#initialize a variable for the current number
+# Initialize a variable for the current number
 number = 1
 
-
 while number <= 1000:
-    
-    if number % 3  == 0 or  number % 5  == 0:
-    
-    #Add multiples to the sum
+    if number % 3 == 0 or number % 5 == 0:
+        # Add multiples to the sum
         sum_of_multiples += number
 
-#increament of  the number for the next alteration
-number += 1
+    # Increment the number for the next iteration
+    number += 1
 
-
-print(f"the sum of multiples of 3 and 5 below 1000: {sum_of_multiples}")
-
-
-    
-    
+print(f"The sum of multiples of 3 and 5 below 1000: {sum_of_multiples}")

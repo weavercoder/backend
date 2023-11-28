ee9ef36ad6147070f2373e4dd3ea5a095fcb0ec8
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-28 06:55:46 -0500

    edited_new

--- name_user.py
+++ name_user.py
@@ -1,6 +1,5 @@
-name=input("Enter your name?:  ")
-times=input("how many do you want your name printed out:  ")
 
-for i in range(10):
-    print(f'{name}*{times}')
- 
+times=eval(input("how many do you want your name printed out:  "))
+
+for i in range(times):
+    print('EDWIN==',end='')

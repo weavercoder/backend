29421c00e605026f5ffd8e3c3d7abd7be0a936b7
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-28 08:54:22 -0500

    appended

---
+++ grades.py
@@ -0,0 +1,14 @@
+#grades
+grade = eval(input('enter grade'))
+if grade >= 90:
+    print('A') 
+if grade >=80 and grade < 90:
+    print('B')
+if grade >=70 and grade < 80:
+    print('C')
+if grade >=60 and grade < 70:
+    print('D')
+if grade <60:
+    print('F')    
+
+
---
+++ multiples.py
@@ -0,0 +1,18 @@
+#A program that gets the multiples of 3 or 5 below 1000
+
+#derive a variable
+sum_of_multiples=0
+
+#set a counter
+count=1
+
+#logical function
+
+while count > 1000:
+    if count % 3 == 0 or count % 5 == 0
+
+    sum_of_multiples += count
+
+    #increament count to move to the next number
+    count += 1
+print(f'the sum of multiples of 3 or 5: {sum_of_multiples}')

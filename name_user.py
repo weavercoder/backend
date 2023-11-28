596f86a18f86fd46fe1dc585338124ef39f0efe0
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-28 06:02:12 -0500

    edited.py

---
+++ name_user.py
@@ -0,0 +1,6 @@
+name=input("Enter your name?:  ")
+times=input("how many do you want your name printed out:  ")
+
+for i in range(10):
+    print(f'{name}*{times}')
+ 
---
+++ numbers.py
@@ -0,0 +1,7 @@
+#5 numbers separated by 3 spaces
+na = eval(input('Enter first number: '))
+n2 = eval(input('Enter second number: '))
+n3 = eval(input('Enter third number: '))
+n4 = eval(input('Enter fourth number: '))
+n5 = eval(input('Enter fifth number: '))
+print(na, n2, n3, n4, n5, sep='   ')

8fa4db697b6e231a113b9b99d403f754c69b3f75
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-29 12:13:31 -0500

    added

---
+++ random100.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+for i in range(100):
+
+x =randint (3,5)
+
+if x == 3
+ print()
+
+else:
+ print()
--- random_num.py
+++ random_num.py
@@ -2,6 +2,7 @@
 
 from random import randint
 x = randint(1,100)
+
 print('A random number between 1 and 10  is', x)
 print()
 

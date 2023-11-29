270526e2a21b4eb26e44f139f62e9d8691d4c3d9
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-29 11:25:00 -0500

    edited_new

--- random_num.py
+++ random_num.py
@@ -1,3 +1,6 @@
+#python comes with a module called random ,that allows us to use random numbers in our programs
+
 from random import randint
-x = randint(1,10)
+x = randint(1,100)
 print('A random number between 1 and 10  is', x)
+print()

2bcf7d01907efd0a456e028073cfc3c70b00f423
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-29 09:19:55 -0500

    add_new

--- rectanglex.py
+++ rectanglex.py
@@ -1,6 +1,9 @@
 
 
 
-for i in range(10):
+'''for i in range(10):
     print('*'*100)
-print()
+print()'''
+
+for i in range(100):
+    print('*'* (i+1))
--- temp.py
+++ temp.py
@@ -1,32 +1,32 @@
 #A program that asks temparature in celcius input give a message depending on the temparature
 
 
-temp=input("Enter the temparature in celcius     :")
+x=input("Enter the temparature in celcius     :")
 
-if temp  > = -273.15:
+if x > = -273.15:
 
     print("The temparature ia invalid because is because it is below absolute zero    :")
 
-elif temp == -273.15:
+elif x == -273.15:
 
     print("The temparature is absolute zero        :")
 
-elif temp  = <-273.15 and  > = 0:
+elif x < = -273.15 and x  > = 0:
 
     print("The temparature is freezing point       :")
 
-elif temp == 0:
+elif x == 0:
 
     print("The temparature is at freezing point    :")
 
-elif temp   = <  0 and  = >  100:
+elif x  = <  0 and   x = >  100:
 
     print("The temparature is in normal range      :")
 
-elif temp == 100:
+elif x == 100:
 
     print("The temparature is at boiling point     :")
 
-elif temp  > = 100:
+elif x > = 100:
 
     print("The temparature is above boiling point  :")

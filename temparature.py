152e55097c0349fa54282f74246fc2208e546d0c
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-29 02:25:31 -0500

    add_new

--- temparature.py
+++ temparature.py
@@ -1,17 +1,19 @@
-temp=eval(input('Enter temparature     : '))
-units=(input('Celcius or fahrenheit?,   :'))
+temp=eval(input('Enter temparature   : '))
+unit=eval(input('ferenheit or celcius    :'))
+f_temp = 9/5 *temp +32
+c_temp = 5/9 * temp - 32
+
+if unit == ('celcius'):
+
+    f_temp = 9/5 *temp +32
+    
+    print(f'the temparature in farenheit,{f_temp}')
+
+elif unit == ('ferenheit'):
+    c_temp = 5/9 * temp - 32
+    print(f'the temparature in celcius,{c_temp}')
 
 
-if units == bool(0):
-    celcius = {units}
-    f_temp = 9/5 * temp+32
-
-    print(f'the temparature in fahrenheit is' , {f_temp} )
-
-else:
-    fahrenheit = {units}
-    c_temp = 5/9 * temp-32
-    print(f'the temparature in celcius' , {c_temp})
 
 
 

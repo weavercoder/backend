3824343cf676bd18bf2b178c514d8bc1a0f99b89
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-29 12:51:00 -0500

    additions

--- temp.py
+++ temp.py
@@ -9,27 +9,29 @@ if x   > -273.15:
 
 elif x == -273.15:
 
-    print("The temparature is absolute zero        :")
+    print("The temparature is absolute zero           :")
 
 elif  x <  -273.15  and x  > 0:
 
-    print("The temparature is freezing point       :")
+    print("The temparature is freezing point          :")
 
 elif x == 0:
 
-    print("The temparature is at freezing point    :")
+    print("The temparature is at freezing point       :")
 
 elif   x <  0  or  x >  100:
 
-    print("The temparature is in normal range      :")
+    print("The temparature is in normal range         :")
 
 elif x == 100:
 
-    print("The temparature is at boiling point     :")
+    print("The temparature is at boiling point         :")
 
 elif x >  100:
 
     print("The temparature is above boiling point  :")
+
+
 
 
 print()

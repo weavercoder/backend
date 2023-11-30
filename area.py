58aec63c6bfb99a08fd99408d39c0913c8a7a755
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-30 18:49:14 -0500

    add

--- area.py
+++ area.py
@@ -4,5 +4,6 @@ width =  float(input("enter the width: "))
 #calculate area
 area = 0.5 * height * width
 
-print(f"The height is {height} and the width is {width}:")
-print(f"The area of the triangle is {area}: ")
+print(f'The height is' ,height, 'and the width is' , width )
+
+print(f'The area of the triangle is' ,area )

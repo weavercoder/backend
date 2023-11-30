23bb1a00b2d9fef64c8954f1a6f8f8aa28d193fc
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-11-30 13:42:20 -0500

    edited_new

--- temp_funct.py
+++ temp_funct.py
@@ -1,8 +1,10 @@
 #convert celcius into farenheit
 
 def convert(t):
+    
     return t*9/5+32
 
+#Ask the user for input
 temp=(eval(input('Enter temparature in celcius      :')))
         
 result=convert(temp)

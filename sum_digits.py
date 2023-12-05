f6855c16b76e72b2c97d67fdfd4172deac9d114a
edwin njerumuchiri <weaverbirdcreed@gmail.com>
2023-12-05 02:46:12 -0500

    add_new

--- sum_digits.py
+++ sum_digits.py
@@ -1,7 +1,14 @@
 
+#A function that is given an integer (num) returns the sum of (num)
 
 def sum_digits(num):
-    
+
+#str(num):: converts the numerical value 'number' into its string repesentation
+#for digit in str(number):iterates over each character in the string representation of the number
+#int(digit)converts each character(digit) back into its integer repesentation(this is necessesary)
+#because when you iterate over a string each character is treated as a string
+#sum(....):add up all the integers obtained fro the previous step
+
     return  sum(int(digit) for digit in str(num))
 
 result =sum_digits(123)

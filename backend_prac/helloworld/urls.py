52f054ae2caae9b312effedd746b463b9cacac47
edwin njeru <weaverbirdcreed@gmail.com>
2023-12-09 12:14:26 -0500

    add_new

--- backend_prac/helloworld/urls.py
+++
@@ -1,7 +0,0 @@
-# helloworld/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('hello/', views.hello_world, name='hello_world'),
-]
--- get_sum.py
+++ get_sum.py
@@ -1,6 +1,23 @@
-def digital_sum(num):
+'''def digital_sum(num):
 
  return sum(int(digit) for  digit  in str(num))
 
 result=digital_sum(45698675)
-print(result)
+print(result)'''
+
+
+'''import random
+
+
+from random import shuffle
+players = ['joe','ben','ken','jane','tom']
+shuffle(players)
+
+for p in players:
+    print(p, 'its your turn')'''
+
+s= 'Hi ! this is a test'
+print(s.split())
+
+
+
